f5dc8ac1c0fd21c294d5c1a7608017eb5d065773
Afsana1313 <afsana201nipa@gmail.com>
2023-08-22 15:31:48 +0600

    Added capsule

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-console.log(camera);
 // this is for rendering. This is must. irrespective of the objects rendered in the screen.
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,7 +27,7 @@ const material = new THREE.MeshBasicMaterial({
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
+//scene.add(texture);
 camera.position.z = 5;
 
 //create a blue LineBasicMaterial
@@ -41,12 +40,30 @@ const linegeometry = new THREE.BufferGeometry().setFromPoints(points);
 const line = new THREE.Line(linegeometry, linematerial);
 scene.add(line);
 ////////////////////////
+
+// load a texture, set wrap mode to repeat
+const texture = new THREE.TextureLoader().load("./public/littol-dog.jpg");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(4, 4);
+// scene.add(texture);
+
+// Capsule
+const capsulegeometry = new THREE.CapsuleGeometry(1.2, 2, 8, 12);
+const capsulematerial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const capsule = new THREE.Mesh(capsulegeometry, capsulematerial);
+scene.add(capsule);
+
 document.body.appendChild(renderer.domElement);
+
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   cube.rotation.z += 0.01;
+  line.rotation.x += 0.01;
+  line.rotation.y += 0.01;
+  line.rotation.z += 0.01;
   renderer.render(scene, camera);
 }
 animate();
