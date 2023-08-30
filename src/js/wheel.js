8fdb3388d5caa3d4d58c3d0491f28778191f3beb
LimaAkter233 <limaakter5691@gmail.com>
2023-08-30 16:28:47 +0600

    tree,background, water body, animation of directional light are added

--- src/js/wheel.js
+++ src/js/wheel.js
@@ -7,7 +7,14 @@ import gsap from 'gsap'
 import { DragControls } from "three/addons/controls/DragControls.js";
 
 
-const rollerSpeed = 0.0004
+const rollerSpeed = 0.000004
+const ringInnerRadius = 250
+const rollerRadius = 8
+const shadowCameraArea = 350;
+const rollerSpeedMultiplier = 30
+const mapShadowSize = 4096
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,12 +22,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color(0xffa500);
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  3000
 );
 
 const orbit = new OrbitControls(camera, renderer.domElement);
@@ -31,7 +38,7 @@ orbit.maxPolarAngle = 1.5
 
 orbit.enableRotate = true
 
-camera.position.set(0, 140, 240);
+camera.position.set(0, 100, -10);
 
 orbit.update();
    //console.log(orbit.getAzimuthalAngle());
@@ -40,10 +47,28 @@ orbit.update();
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
+const waterBodyGeo = new THREE.CircleGeometry(3000, 300,3000)
+const waterBodyMat = new THREE.MeshStandardMaterial({
+  color: 0x006994
+});
+const waterBody = new THREE.Mesh(waterBodyGeo, waterBodyMat)
+scene.add(waterBody)
+waterBody.rotation.x = -0.5 * Math.PI
+waterBody.position.y = -10
+
+// const skyGeo = new THREE.BoxGeometry(30000000, 3000)
+// const skyMat = new THREE.MeshBasicMaterial({
+//   color: 0xffa500,
+//   side: THREE.DoubleSide
+// });
+// const sky = new THREE.Mesh(skyGeo, skyMat)
+// scene.add(sky)
+// sky.position.z = 1000
+
 const textureLoader = new THREE.TextureLoader();
 
 const planeGeo = new THREE.CircleGeometry(300, 300);
-const planeMat = new THREE.MeshStandardMaterial({
+const planeMat = new THREE.MeshPhongMaterial({
   map: textureLoader.load(uranusTexture),
   side: THREE.DoubleSide
 });
@@ -60,44 +85,68 @@ const centerBox = new THREE.Mesh(boxGeo, boxMat);
 
 
 
-const rollerGeo = new THREE.SphereGeometry(8, 800);
+const rollerGeo = new THREE.SphereGeometry(rollerRadius, 800);
 const rollerMat = new THREE.MeshStandardMaterial({
   map: textureLoader.load(earthTexture)
 });
 const roller = new THREE.Mesh(rollerGeo, rollerMat);
-roller.position.x = 50;
+roller.position.x = ringInnerRadius + rollerRadius / 2 ;
 roller.position.y = 8;
 roller.castShadow = true
 centerBox.castShadow = true;
 
 
-const roller1 = new THREE.Mesh(rollerGeo, rollerMat);
-roller1.position.x = 70;
-roller1.position.y = 8;
-roller1.castShadow = true;
-scene.add(roller1)
+const coneBotGeo = new THREE.ConeGeometry(10, 15, 32);
+const coneBotMat = new THREE.MeshPhongMaterial({ color: 0x008000 });
+const coneBot = new THREE.Mesh(coneBotGeo, coneBotMat);
+scene.add(coneBot);
+coneBot.position.y = 15;
+coneBot.position.x = 100;
 
+const coneMidGeo = new THREE.ConeGeometry(8, 15, 32);
+const coneMidMat = new THREE.MeshPhongMaterial({ color: 0xa0c253 });
+const coneMid = new THREE.Mesh(coneMidGeo, coneMidMat);
+coneBot.add(coneMid);
+coneMid.position.y = 10;
 
+const coneTopGeo = new THREE.ConeGeometry(6, 10, 32);
+const coneTopMat = new THREE.MeshPhongMaterial({ color: 0xcfea91 });
+const coneTop = new THREE.Mesh(coneTopGeo, coneTopMat);
+coneBot.add(coneTop);
+coneTop.position.y = 15;
 
-const ringGeo = new THREE.RingGeometry(45, 50, 30)
+const cylinderWoodGeo = new THREE.CylinderGeometry(3, 3, 20, 300, 300)
+const cylinderWoodMat = new THREE.MeshPhongMaterial({ color: 0x310c0c });
+const cylinderWood = new THREE.Mesh(cylinderWoodGeo, cylinderWoodMat)
+coneBot.add(cylinderWood);
+cylinderWood.position.y = -10
+
+coneBot.castShadow = true
+coneMid.castShadow = true
+coneTop.castShadow = true
+cylinderWood.castShadow = true
+
+const ringGeo = new THREE.RingGeometry(ringInnerRadius, ringInnerRadius + rollerRadius,500,500)
 const ringMat = new THREE.MeshStandardMaterial({
   color: 0x00ff00,
   side: THREE.DoubleSide
 });
 const ring = new THREE.Mesh(ringGeo, ringMat)
 ring.rotation.x = -0.5 * Math.PI;
-ring.position.y = 1;
+ring.position.y = 0.5;
+ring.castShadow = true
+ring.receiveShadow = true
 
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 5.0);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 8.0);
 scene.add(directionalLight);
-directionalLight.position.set(300,30,40)
+directionalLight.position.set(140,40,40)
 directionalLight.castShadow = true
-directionalLight.shadow.camera.bottom = -600
-directionalLight.shadow.camera.top = 900;
-directionalLight.shadow.camera.left = -600;
-directionalLight.shadow.camera.right = 600;
-
+directionalLight.shadow.camera.bottom = -shadowCameraArea
+directionalLight.shadow.camera.top = shadowCameraArea+ 200;
+directionalLight.shadow.camera.left = -shadowCameraArea;
+directionalLight.shadow.camera.right = shadowCameraArea;
+directionalLight.shadow.mapSize.width = mapShadowSize
+directionalLight.shadow.mapSize.height = mapShadowSize;
 
 const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
 scene.add(directionalLightHelper)
@@ -113,12 +162,12 @@ function animate() {
   //Self-rotation
   window.addEventListener("keydown", (e) => {
     if (e.key == "ArrowRight") {
-      centerBox.rotateY(0.0001);
-      roller.rotateX(-1 * rollerSpeed);
+      centerBox.rotateY(rollerSpeed);
+      roller.rotateX(-rollerSpeedMultiplier * rollerSpeed);
     }
     if (e.key == "ArrowLeft") {
-        centerBox.rotateY(-0.0001);
-          roller.rotateX(rollerSpeed);
+        centerBox.rotateY(-rollerSpeed);
+          roller.rotateX(rollerSpeedMultiplier * rollerSpeed);
       }
     //   gsap.to(camera.position, {
     //     z: 14,
@@ -128,13 +177,13 @@ function animate() {
     //     }
     //   });
   });
-  orbit.addEventListener("change", () => {
-   // console.log(orbit.getAzimuthalAngle())
-    centerBox.rotateY(0.00001);
-  //  console.log(camera)
-      roller.rotateX(-1 * rollerSpeed);
-  });
-
+  // orbit.addEventListener("change", () => {
+  //   centerBox.rotateY(0.00001);
+  //     roller.rotateX(-1 * rollerSpeed);
+  // });
+  const time = Date.now() * 0.0005;
+  directionalLight.position.x = Math.sin(time * 0.7) * 270 ;
+  directionalLight.position.z = Math.cos(time * 0.7) * 270 ;
 
   renderer.render(scene, camera);
 }
